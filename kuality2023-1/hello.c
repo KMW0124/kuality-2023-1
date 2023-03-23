d4cccf8a96d4d8d30b69578c645da34991bcf5c4
MinWoo Kim <k3simple@naver.com>
2023-03-23 18:08:01 +0900

    Add week03 project

--- kuality2023-1/hello.c
+++ kuality2023-1/hello.c
@@ -1,6 +1,28 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <stdio.h>
 
-int main()
-{	
-	printf("Hello world\n");
+int main() {
+
+	int n1, n2, n3;
+	char c;
+	printf("input number : ");
+	scanf(" %d %d %d", &n1, &n2, &n3);
+	printf("input oprerator : ");
+	scanf(" %c", &c);
+	switch (c) {
+	case '*':
+		printf("n1 * n2 * n3 = %d\n", n1 * n2 * n3);
+		break;
+	case '+':
+		printf("n1 + n2 + n3 = %d\n", n1 + n2 + n3);
+		break;
+	case '-':
+		printf("n1 - n2 - n3 = %d\n", n1 - n2 - n3);
+		break;
+	case '/':
+		printf("n1 / n2 / n3 = %d\n", n1 / n2 / n3);
+	default:
+		printf("잘못 입력 되었습니다\n");
+	}
 }
